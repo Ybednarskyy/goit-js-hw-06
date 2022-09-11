7508bd6344e82358cbee45f6ed586f6a93926a6d
Ybednarskyy <y.bednarskyy@gmail.com>
2022-09-11 22:11:22 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  const formElements = event.currentTarget.elements;
+
+  const formData = {
+    email: formElements.email.value,
+    password: formElements.password.value,
+  };
+  console.log(formData);
+
+  event.currentTarget.reset();
+}
