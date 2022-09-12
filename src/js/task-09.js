69f9d73ead229cee49442a8be19b1a96e6cea6d2
Ybednarskyy <y.bednarskyy@gmail.com>
2022-09-12 08:56:29 +0300

    9 task finished

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnRef = document.querySelector(".change-color");
+const currentColorOutputRef = document.querySelector(".color");
+
+changeColorBtnRef.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  currentColorOutputRef.textContent = getRandomHexColor();
+});
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  let arrayOfElements = [];
+
+  for (let index = 0; index < amount; index++) {
+    const element = document.createComment("div");
+    arrayOfElements.push(element);
+    console.log(element);
+  }
+  console.log(arrayOfElements);
+  return arrayOfElements;
+}
+
+createBoxes(2);
